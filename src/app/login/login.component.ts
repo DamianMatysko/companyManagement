d7eb17223c54d4547d682dc7a5c2bc86fde0beb2
DamianMatysko <mada11@azet.sk>
2021-02-09 01:32:02 +0100

    workable prototype v3.1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,10 @@ import {first} from 'rxjs/operators';
 import {AlertService} from '../services/alert.service';
 import {AuthenticationService} from '../services/authentication.service';
 
-@Component({templateUrl: 'login.component.html'})
+@Component({
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.css']
+})
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loading = false;
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,8 +6,10 @@ import {AuthenticationService} from '../services/authentication.service';
 import {EmployeeService} from '../services/employee.service';
 import {AlertService} from '../services/alert.service';
 
-@Component({templateUrl: 'register.component.html'})
-
+@Component({
+  templateUrl: 'register.component.html',
+  styleUrls: ['register.component.css']
+})
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
